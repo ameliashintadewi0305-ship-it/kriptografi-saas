8d6ac08c54a00d44866c8e03acdc5e7f161223af
Amelia <ameliashintadewi0305@gmail.com>
2025-08-11 09:07:38 +0700

    Fix Dockerfile to run init_db.py

--- init_db.py
+++ init_db.py
@@ -1,9 +1,8 @@
 from app import app, db
 import os
-# Membuat direktori /data jika belum ada
-if not os.path.exists('/data'):
-    os.makedirs('/data')
+
 with app.app_context():
+    # Coba untuk membuat tabel jika belum ada
     try:
         db.create_all()
         print("Database tables created successfully!")
