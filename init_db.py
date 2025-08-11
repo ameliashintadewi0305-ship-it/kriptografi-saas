80845d8eb1fa5718a91bbee8770e9673b530eb29
Amelia <ameliashintadewi0305@gmail.com>
2025-08-11 08:56:44 +0700

    Fix: Use init_db.py to create database on build

--- init_db.py
+++ init_db.py
@@ -1,6 +1,8 @@
 from app import app, db
 import os
-
+# Membuat direktori /data jika belum ada
+if not os.path.exists('/data'):
+    os.makedirs('/data')
 with app.app_context():
     try:
         db.create_all()
